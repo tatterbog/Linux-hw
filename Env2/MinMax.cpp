c863a7c7e8e70c6c8db8d786bda3e421e7cbdf99
tattierbog <emilia.aramyan64@gmail.com>
2025-09-14 08:53:19 -0700

    MinMax update

--- Env2/MinMax.cpp
+++ Env2/MinMax.cpp
@@ -2,8 +2,30 @@
 #include <cstdlib>
 
 int main(int argc, char* argv[]){
+
 	if(argc != 4){
-		return 0;
+		std::cout << "Usage" << argv[0] << " num1 num2 num3\n";
+		return 1;
+	}
+	
+	for(int i = 1; i < 3; i++){
+		char* a = argv[i];
+	        if(*a == '-'){
+			a++;
+		}
+
+		if(*a == '\0'){
+			std::cout << "not a valid number\n";
+			return 2;
+		}
+
+		while(*a){
+			if(*a > '9' || *a < '0'){
+				std::cout << "Arguments must have an integer value\n"; 
+			return 3;
+			}
+			*a++;
+		}
 	}
 
 	int min = atoi(argv[1]), max = min;

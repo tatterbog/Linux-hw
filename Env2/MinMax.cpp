b207391c9d4884a1b612448b4cce343617be6c1d
tattierbog <emilia.aramyan64@gmail.com>
2025-09-14 05:55:52 -0700

    Env2

---
+++ Env2/MinMax.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char* argv[]){
+	if(argc < 2){
+		return 0;
+	}
+
+	int min = atoi(argv[1]), max = min;
+
+	for(int i = 2; i < argc; i++){
+		if(min > atoi(argv[i])){
+			min = atoi(argv[i]);
+		}
+
+		if(max < atoi(argv[i])){
+                        max = atoi(argv[i]);
+                }
+	}
+
+	std::cout << "Min: " << min << ", Max: " << max << "\n";
+}
---
+++ Env2/Rev.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char* argv[]){
+
+	for(int i = 0; i < argc/2; i++){
+		auto t = argv[i];
+	       	argv[i] = argv[argc - i - 1];
+		argv[argc - i - 1] = t;	
+	}
+
+	for(int i = 0; i < argc - 1; i++){
+		std::cout << argv[i] << ' ';
+	}
+	std::cout << "\n";
+}

f138c21bcf4885a36ca6ac317750d9995712a218
tatterbog <emilia.aramyan64@gmail.com>
2025-10-21 20:30:32 +0400

    interactive-shell.cpp

--- PS-2/interactive-shell.cpp
+++ PS-2/interactive-shell.cpp
@@ -6,6 +6,60 @@
 #include <sys/wait.h>
 #include <sstream>
 
+int forkCommands(char **args, int fd, bool silent) {
+    pid_t pid = fork();
+    if (pid == -1){
+        std::cerr << "Fork error\n";
+        return 1;
+    }
+
+    if (pid == 0){
+        if (silent){
+            std::string file = std::to_string(getpid()) + ".log";
+            int fd2 = creat(file.c_str(), 0644);
+            if (fd2 == -1) {
+                std::cerr << "Creating error\n";
+                exit(1);
+            }
+            dup2(fd2, 1);
+            dup2(fd2, 2);
+            close(fd2);
+        } 
+		else if (fd != -1) {
+            dup2(fd, 1);
+            close(fd);
+        }
+		
+        const char *old = getenv("PATH");
+        if (!old){
+			old = "";
+		}
+		
+        std::string newPath = std::string(".:") + old;
+        setenv("PATH", newPath.c_str(), 1);
+
+        execvp(args[0], args);
+        std::cerr << "Exec error\n";
+        exit(1);
+    }
+
+    int status;
+    waitpid(pid, &status, 0);
+    if (fd != -1){
+        close(fd);
+	}
+	
+    if (WIFEXITED(status)){
+        return WEXITSTATUS(status);
+	}
+		
+    else if (WIFSIGNALED(status)){
+        return WTERMSIG(status);
+	}
+	
+    return -1;
+}
+
 int main(int argc, char** argv){
 	std::string str;
 	while(true){
@@ -37,7 +91,9 @@ int main(int argc, char** argv){
 		for(int i = 0; i < words.size(); ){
 			std::vector<char *> args;
 			int fd = -1;
+			bool silent = false;
 			int j = i;
+			
 			for(; j < words.size() && words[j] != ";" && words[j] != "&&" && words[j] != "||"; j++){
 				if((words[j] == ">" || words[j] == ">>") && j + 1 < words.size()){
 					int flags = O_CREAT | O_WRONLY;
@@ -54,6 +110,9 @@ int main(int argc, char** argv){
 					}
 					j++;
 				}
+				else if(words[j] == "silent"){
+					silent = true;
+				}
 				else{
 					args.push_back(&words[j][0]);
 				}
@@ -65,81 +124,21 @@ int main(int argc, char** argv){
 				break;
 			}	
 
-			pid_t pid = fork();
-			if(pid == -1){
-				std::cerr << "Fork error\n";
-        	    return 1;
-			}
-
-			if(pid == 0){
-				int index = 0;
-				if(std::string(args[0]) == "silent"){
-					index = 1;
-					if(!args[1]){
-						std::cerr << "nothing to run\n";
-						exit(1);
-					}
-
-					std::string file = std::to_string(getpid()) + ".log";
-					int fd2 = creat(file.c_str(), 0644);
-					if(fd2 == -1){
-						std::cerr << "Creating error\n";
-						exit(1);
-					}
-
-					dup2(fd2, 1);
-					dup2(fd2, 2);
-					close(fd2);
-				}
-
-				else if(fd != -1){
-					dup2(fd, 1);
-					close(fd);
-				}
-
-				const char* old = getenv("PATH");
-				if(!old){
-					old = "";
-				}
-          	    std::string newP = std::string(".:") + old;
-                setenv("PATH", newP.c_str(), 1);
-			
-				execvp(args[index], &args[index]);
-				std::cerr << "Exec error\n";
-                        	exit(1);
-			}
-
-			else{
-				int status;
-				if(waitpid(pid, &status, 0) == -1){
-					std::cerr << "WaitPid error\n";
-	        	                return 1;
-				}
-		 		
-				if(fd == -1){
-					close(fd);	
-				}	
-
-				int exit = WIFEXITED(status);
-				std::cout << "Exit status: " << exit << '\n';
+			int exit = forkCommands(args.data(), fd, silent);	
 				
-				
-				if(j < words.size()){
-					if(words[j] == "&&" && exit != 0){
+			if(j < words.size()){
+				if(words[j] == "&&" && exit != 0){
+					j++;
+					while(j < words.size() && words[j] != ";" && words[j] != "&&" && words[j] !="||"){
 						j++;
-						while(j < words.size() && words[j] != ";" && words[j] != "&&" && words[j] !="||"){
-							j++;
-						}	
-					}
+					}	
+				}
 				
-					else if(words[j] == "||" && exit == 0){
-						j++;
-						while(j < words.size() && words[j] != ";" && words[j] != "&&" && words[j] !="||"){                                                	       
+				else if(words[j] == "||" && exit == 0){
+					j++;
+					while(j < words.size() && words[j] != ";" && words[j] != "&&" && words[j] !="||"){                                                	       
 							j++;
-                        }
-	
 					}
-
 				}
 			}
 			i = j;

23e4999af6d0e154aea16ff0903f55dbf8cbbd22
tattierbog <emilia.aramyan64@gmail.com>
2025-11-23 10:22:44 -0800

    TH-2

---
+++ TH-2/parallel_scheduler.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <iostream>
+#include <unistd.h>
+#include <pthread.h>
+#include <queue>
+
+
+class parallel_scheduler{
+private:
+	int capacity;
+	bool stop = false;
+
+	pthread_t* tds;
+	pthread_mutex_t mutex;
+	pthread_cond_t cond;
+	
+	struct Func{
+		void (*func)(void*);
+		void *arg;
+	};	
+		
+	std::queue<Func> funcs;
+		
+	static void* worker_entry(void* arg);
+
+
+	void worker_loop();
+
+public:
+	explicit parallel_scheduler(int cap);
+
+	void run(void (*func)(void*), void* arg);
+	
+	~parallel_scheduler();
+};
+

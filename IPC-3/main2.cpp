f9858b356f2f8a2ae59412c51dd62c7fe0e67f3a
tattierbog <emilia.aramyan64@gmail.com>
2025-12-14 06:42:20 -0800

    IPC-3

--- IPC-3/main2.cpp
+++ IPC-3/main2.cpp
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include "shared_array.h"
 
+
 sem_t* get_semaphore(const std::string& str){ 
 	std::string name = '/' + str + "_sem";
 	return sem_open(name.c_str(), O_CREAT, 0666, 1);
@@ -11,14 +12,14 @@ sem_t* get_semaphore(const std::string& str){
 
 int main(){
 	shared_array arr("arr", 10);
+
 	sem_t* sem = get_semaphore("arr");
-	
 	if(sem == SEM_FAILED){
                 perror("Semaphore");
                 exit(EXIT_FAILURE);
         }
+
 	
-	int i = 0;
 	while(true){
 		sem_wait(sem);
 		std::cout << arr[0] << ' ' << arr[1] << '\n';
